d6aa89d5f7b56f17abc09d14324912903dbcfdc9
jasmosez <jas.schaffer@gmail.com>
2020-01-04 23:56:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let statement;
+
+  if (someValue <= 400) {
+    statement = 'This one is on me!';
+  }
+
+  if (someValue > 2000) {
+    statement = 'I will gladly take your thirty bucks.';
+  }
+  
+  if (someValue > 2500) {
+    statement = 'No can do.';
+  }
+
+  return statement;
+
+  // switch (someValue) {
+  //   case (someValue <= 400):
+  //     statement = 'This one is on me!';
+  //     break;
+  //   case (someValue > 2000):
+  //     statement = 'I will gladly take your thirty bucks.';
+  //     break;
+  //   case (someValue > 2500):
+  //     statement = 'No can do.';
+  //     break;
+  // } 
+  // return statement;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let response; 
+
+  (city == 'NYC') ? (response = 'Ok, sounds good.') : (response = 'No go.');
+
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(desc) {
   // Write your code here!
+  
+  let response
+
+  if (desc === 'generous') {
+    response = 'Thank you so much.';
+  } else if (desc === 'not as generous') {
+    response = 'Thank you.';
+  } else {
+    response = 'Bye.';
+  }
+  
+  return response;
+
+  // switch (desc) {
+  //   case "generous":
+  //     response = "Thank you so much.";
+  //     break;
+  //   case "not as generous":
+  //     response = "Thank you";
+  //     break;
+  //   default:
+  //     response = "Bye.";
+  // }
+
+  // return response;
+
 }
