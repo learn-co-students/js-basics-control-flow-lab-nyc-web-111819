6307b2b068fd5a702b5f0f7338e6366365d4601f
Your Name <samanbatool08@gmail.com>
2020-01-06 10:14:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet) {
+    if (feet <= 400) {
+        return 'This one is one me!';
+    } else if (feet > 1999 && feet < 2500) {
+        return 'I will gladly take your thirty bucks.';
+    } else {
+        'No can do';
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+    function ternaryCheckCity(city) {
+        return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+    }
+
+
+
+    function switchOnCharmFromTip(tip) {
+        if (tip = 'generous') {
+            return 'Thank you so much.';
+        } else if (tip = 'not as generous') {
+            return 'Thank you.';
+        } else {
+            'Bye.';
+        }
+    }
